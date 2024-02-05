087ea6096f550763cf2cdf8be7d008487c68f5ef
phamtrananh <phamtrananh1088@gmail.com>
2024-02-05 17:08:06 +0700

    WinMacOs: NoteBookElementary

--- Windows-MacOs/Windows-MacOs/Scripts/Pages/Manager/notebookelementary.js
+++ Windows-MacOs/Windows-MacOs/Scripts/Pages/Manager/notebookelementary.js
@@ -8,6 +8,7 @@
     };
 
     SVG.prototype = {
+        name: 'TorihikiSVGPath',
         curPath: null,
         _initialize: function (conf) {
             if (conf !== undefined) {
@@ -26,96 +27,29 @@
 
             return true;
         },
-        addPath: function (conf) {
-            var self = this;
-            var options = {
-                x: 0,
-                y: 0,
-                x1: 0,
-                y1: 0,
-                x2: 0,
-                y2: 0,
-                ex: 0,
-                ey: 0,
-                width: 0,
-                height: 0,
-                stroke: 'black',
-                fill: 'transparent',
-                strokeWidth: 1
-            };
-            $.extend(options, conf, true);
-
-            var Path = $(`<path d="M ${options.x} ${options.y}" stroke="${options.stroke}" fill="${options.fill}" stroke-width="${options.strokeWidth}" />`);
-            //Path.attr('data-d', options);
-            //Path.data('l', [1,1]);
-            Path.data('p', options);
-            $(self.conf.target).append(Path);
-            // Listen for the event.
-            Path.bind('refresh', async function (evt) {
-                console.log(evt);
-                //if (Math.abs(evt.detail.newValue[0] - evt.detail.oldValue[0]) >= 4 || Math.abs(evt.detail.newValue[1] - evt.detail.oldValue[1]) >= 4) {
-                //    $(this).remove();
-                //    $(self.conf.target).append($(this));
-                //}
-            });
-            self.curPath = Path;
-            return Path;
-        },
-        editPath: function (conf) {
+        add: function (conf) {
             var self = this;
             var Path = self.curPath;
-            if (Path == null) {
-                return;
-            }
-            var options = {
-                x: 0,
-                y: 0,
-                x1: 0,
-                y1: 0,
-                x2: 0,
-                y2: 0,
-                ex: 0,
-                ey: 0,
-                width: 0,
-                height: 0,
-                stroke: 'black',
-                fill: 'transparent',
-                strokeWidth: 1
-            };
-            $.extend(options, conf, true);
-            var p0 = Path.data('p');
-            options.x1 = 2 * p0.ex - p0.x2, options.x2 = options.x1;
-            options.y1 = 2 * p0.ey - p0.y2, options.y2 = options.y1;
-
-            Path.attr('d', `${Path.attr('d')} C ${options.x1} ${options.y1}, ${options.x2} ${options.y2}, ${options.ex} ${options.ey}`);
-            //Path.data('l', [l1, l2]);
-            Path.data('p', options);
-
-            var iframe = document.getElementById('iframeSVG');
-            var html = `<html><body>
-            ${$(self.conf.target).prop('outerHTML')}
-            </body></html>`;
-            iframe.src = 'data:text/html;charset=utf-8,' + encodeURIComponent(html);
-            //var resfreshData = {
-            //    oldValue: [Number(Path.attr('data-width')), Number(Path.attr('data-height'))],
-            //    newValue: [options.width, options.height]
-            //}
-            //var refreshEvent = new CustomEvent("refresh", { detail: resfreshData });
-            // Dispatch the event.
-            //Path[0].dispatchEvent(refreshEvent);
+            return Path;
+        },
+        edit: function (conf) {
+            var self = this;
+            var Path = self.curPath;
             return Path;
         },
     };
-    global.TorihikiSVG = SVG;
+    global.TorihikiSVGPath = SVG;
 
 })(jQuery, this);
+
 (function ($, global) {
     var SVG = function () {
         this._initialize.apply(this, arguments);
+        this.name = 'TorihikiSVGPathGuide';
     };
 
-    SVG.prototype = TorihikiSVG.prototype
-    SVG.prototype.addPath = function (conf) {
+    SVG.prototype = Object.create(TorihikiSVGPath.prototype);
+    SVG.prototype.add = function (conf) {
         var self = this;
         var options = {
             x: 0,
@@ -135,7 +69,9 @@
         $.extend(options, conf, true);
 
         var Path = $(`<path d="M ${options.x} ${options.y}" stroke="${options.stroke}" fill="${options.fill}" stroke-width="${options.strokeWidth}" />`);
-
+        //Path.attr('data-d', options);
+        //Path.data('l', [1,1]);
+        Path.data('p', options);
         $(self.conf.target).append(Path);
         // Listen for the event.
         Path.bind('refresh', async function (evt) {
@@ -147,8 +83,8 @@
         });
         self.curPath = Path;
         return Path;
-    }
-    SVG.prototype.editPath = function (conf) {
+    };
+    SVG.prototype.edit = function (conf) {
         var self = this;
         var Path = self.curPath;
         if (Path == null) {
@@ -170,7 +106,95 @@
             strokeWidth: 1
         };
         $.extend(options, conf, true);
-       
+        var p0 = Path.data('p');
+        options.x1 = 2 * p0.ex - p0.x2, options.x2 = options.x1;
+        options.y1 = 2 * p0.ey - p0.y2, options.y2 = options.y1;
+
+        Path.attr('d', `${Path.attr('d')} C ${options.x1} ${options.y1}, ${options.x2} ${options.y2}, ${options.ex} ${options.ey}`);
+        //Path.data('l', [l1, l2]);
+        Path.data('p', options);
+
+        var iframe = document.getElementById('iframeSVG');
+        var html = `<html><body>
+            ${$(self.conf.target).prop('outerHTML')}
+            </body></html>`;
+        iframe.src = 'data:text/html;charset=utf-8,' + encodeURIComponent(html);
+        //var resfreshData = {
+        //    oldValue: [Number(Path.attr('data-width')), Number(Path.attr('data-height'))],
+        //    newValue: [options.width, options.height]
+        //}
+        //var refreshEvent = new CustomEvent("refresh", { detail: resfreshData });
+        // Dispatch the event.
+        //Path[0].dispatchEvent(refreshEvent);
+        return Path;
+    };
+    global.TorihikiSVGPathGuide = SVG;
+
+})(jQuery, this);
+
+(function ($, global) {
+    var SVG = function () {
+        this._initialize.apply(this, arguments);
+        this.name = 'TorihikiSVGPath1';
+    };
+
+    SVG.prototype = Object.create(TorihikiSVGPath.prototype);
+    SVG.prototype.add = function (conf) {
+        var self = this;
+        var options = {
+            x: 0,
+            y: 0,
+            x1: 0,
+            y1: 0,
+            x2: 0,
+            y2: 0,
+            ex: 0,
+            ey: 0,
+            width: 0,
+            height: 0,
+            stroke: 'red',
+            fill: 'transparent',
+            strokeWidth: 1
+        };
+        $.extend(options, conf, true);
+
+        var Path = $(`<path d="M ${options.x} ${options.y}" stroke="${options.stroke}" fill="${options.fill}" stroke-width="${options.strokeWidth}" />`);
+
+        $(self.conf.target).append(Path);
+        // Listen for the event.
+        Path.bind('refresh', async function (evt) {
+            console.log(evt);
+            //if (Math.abs(evt.detail.newValue[0] - evt.detail.oldValue[0]) >= 4 || Math.abs(evt.detail.newValue[1] - evt.detail.oldValue[1]) >= 4) {
+            //    $(this).remove();
+            //    $(self.conf.target).append($(this));
+            //}
+        });
+        self.curPath = Path;
+        return Path;
+    };
+    SVG.prototype.edit = function (conf) {
+        var self = this;
+        var Path = self.curPath;
+        if (Path == null) {
+            return;
+        }
+        var options = {
+            x: 0,
+            y: 0,
+            x1: 0,
+            y1: 0,
+            x2: 0,
+            y2: 0,
+            ex: 0,
+            ey: 0,
+            width: 0,
+            height: 0,
+            stroke: 'black',
+            fill: 'transparent',
+            strokeWidth: 1
+        };
+        $.extend(options, conf, true);
+
         Path.attr('d', `${Path.attr('d')} L ${options.ex} ${options.ey}`);
 
         var iframe = document.getElementById('iframeSVG');
@@ -186,10 +210,11 @@
         // Dispatch the event.
         //Path[0].dispatchEvent(refreshEvent);
         return Path;
-    }
-    global.TorihikiSVG1 = SVG;
+    };
+    global.TorihikiSVGPath1 = SVG;
 
 })(jQuery, this);
+
 (function ($, global) {
     var DIV = function () {
         this._initialize.apply(this, arguments);
@@ -217,7 +242,7 @@
             return true;
         },
 
-        addPath: function (conf) {
+        add: function (conf) {
             var self = this;
             var options = {
                 x: 0,
@@ -246,7 +271,7 @@
             return Path;
         },
 
-        editPath: function (conf) {
+        edit: function (conf) {
             var self = this;
             var Path = self.curPath;
 
@@ -320,13 +345,17 @@ var notebookelementary01 = {
 
 class PathAction extends Action {
     svg;
+    svgGuide;
     content;
     actionType;
     p;
     pathTypes;
     constructor(svgTarget, divTarget, pathTypes) {
         super();
-        this.svg = new TorihikiSVG1({
+        this.svg = new TorihikiSVGPath1({
+            target: svgTarget,
+        });
+        this.svgGuide = new TorihikiSVGPathGuide({
             target: svgTarget,
         });
         this.content = new TorihikiDIV({
@@ -342,7 +371,7 @@ class PathAction extends Action {
         if (evt.type === 'pointerdown') {
             //this.actionType = Enum.ActionTypes.select;
             this.p = new Selection(evt.offsetX, evt.offsetY);
-            const cPath = this.content.addPath({
+            const cPath = this.content.add({
                 x: this.p.x,
                 y: this.p.y,
                 x1: this.p.x,
@@ -352,7 +381,17 @@ class PathAction extends Action {
                 ex: this.p.x,
                 ey: this.p.y,
             });
-            const svgPath = this.svg.addPath({
+            const svgPath = this.svg.add({
+                x: this.p.x,
+                y: this.p.y,
+                x1: this.p.x,
+                y1: this.p.y,
+                x2: this.p.x,
+                y2: this.p.y,
+                ex: this.p.x,
+                ey: this.p.y,
+            });
+            const svgPathGuide = this.svgGuide.add({
                 x: this.p.x,
                 y: this.p.y,
                 x1: this.p.x,
@@ -370,7 +409,7 @@ class PathAction extends Action {
             }
             this.p.eX = evt.offsetX;
             this.p.eY = evt.offsetY;
-            const cPath = this.content.editPath({
+            const cPath = this.content.edit({
                 x: this.p.x,
                 y: this.p.y,
                 x1: 0,
@@ -382,7 +421,7 @@ class PathAction extends Action {
                 ex: this.p.eX,
                 ey: this.p.eY,
             });
-            const svgPath = this.svg.editPath({
+            const svgPath = this.svg.edit({
                 x: this.p.x,
                 y: this.p.y,
                 x1: 0,
@@ -394,7 +433,23 @@ class PathAction extends Action {
                 ex: this.p.eX,
                 ey: this.p.eY,
             });
-
+            const svgPathGuide = this.svgGuide.edit({
+                x: this.p.x,
+                y: this.p.y,
+                x1: 0,
+                y1: 0,
+                x2: 0,
+                y2: 0,
+                width: this.p.width,
+                height: this.p.height,
+                ex: this.p.eX,
+                ey: this.p.eY,
+            });
+        //    var iframe = document.getElementById('iframeSVG');
+        //    var html = `<html><body style="margin: 0;">
+        //${$(this.svgTarget).prop('outerHTML')}
+        //</body></html>`;
+        //    iframe.src = 'data:text/html;charset=utf-8,' + encodeURIComponent(html);
             BasicFunction.editHistory([cPath, svgPath]);
         }
         if (evt.type === 'pointerup' || evt.type === 'pointerout') {
