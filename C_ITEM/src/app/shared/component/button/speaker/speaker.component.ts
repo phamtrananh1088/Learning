57b1b49351b96efe7873da0af60cdd15c8d89109
phamtrananh1088 <phamtrananh1088@gmail.com>
2024-04-07 22:22:37 +0700

    animation

---
+++ C_ITEM/src/app/shared/component/button/speaker/speaker.component.ts
@@ -0,0 +1,63 @@
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Animation, AnimationController, IonButton } from '@ionic/angular';
+
+@Component({
+  selector: 'app-speaker',
+  templateUrl: './speaker.component.html',
+  styleUrls: ['./speaker.component.scss'],
+})
+export class SpeakerComponent  implements OnInit {
+  @ViewChild(IonButton, { read: ElementRef }) button?: ElementRef<HTMLIonButtonsElement>;
+  @ViewChild('graphA', { static: true }) graphA?: ElementRef;
+  @ViewChild('graphB', { static: true }) graphB?: ElementRef;
+  private animationButton?: Animation;
+  private animationA?: Animation;
+  private animationB?: Animation;
+  constructor(
+    private animationCtrl: AnimationController
+  ) { }
+
+  ngOnInit() {}
+
+  ngAfterViewInit() {
+    setTimeout(() => {
+      this.animationButton = this.animationCtrl
+        .create()
+        .addElement(this.button!.nativeElement)
+        .duration(100)
+        .iterations(1)
+        .keyframes([
+          { offset: 0, width: '95px', height: '95px' },
+          { offset: 1, width: '100px', height: '100px' },
+        ]);
+      this.animationA = this.animationCtrl
+        .create()
+        .addElement(this.graphA!.nativeElement)
+        .duration(500)
+        .iterations(1)
+        .keyframes([
+          { offset: 0, display: 'none'},
+          { offset: 0.5, display: 'block' },
+        ])
+        .fromTo('transform', 'matrix(1.3600000143051147,0,0,1.3600000143051147,516.219970703125,522.4000244140625) translateX(-5px) scale(0.5,0.5)', 'matrix(1.3600000143051147,0,0,1.3600000143051147,516.219970703125,522.4000244140625) translateX(0px)')
+        .fromTo('opacity','0.5','1');
+        this.animationB = this.animationCtrl
+        .create()
+        .addElement(this.graphB!.nativeElement)
+        .duration(500)
+        .iterations(1)
+        // .keyframes([
+        //   { offset: 0, transform: 'matrix(1.3600000143051147,0,0,1.3600000143051147,516.219970703125,522.4000244140625) translateX(-5px) scale(0.5,0.5)'},
+        //   { offset: 0.5, transform: 'matrix(1.3600000143051147,0,0,1.3600000143051147,516.219970703125,522.4000244140625) translateX(0px)' },
+        // ])
+        .fromTo('transform', 'matrix(1.3600000143051147,0,0,1.3600000143051147,516.219970703125,522.4000244140625) translateX(-5px) scale(0.5,0.5)', 'matrix(1.3600000143051147,0,0,1.3600000143051147,516.219970703125,522.4000244140625) translateX(0px) ')
+        .fromTo('opacity','0','1');
+    }, 0);
+  }
+
+  click () {
+    this.animationButton?.play();
+    this.animationA?.play();
+    this.animationB?.play();
+  }
+}
